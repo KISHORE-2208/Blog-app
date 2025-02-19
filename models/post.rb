d6d9f669700efe99da227e0872b93e44dc5b1d9c
KISHORE-2208 <kishoregunasekaran22@gmail.com>
2025-02-19 17:39:47 +0530

    Add files via upload

---
+++ models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+    has_one_attached :image
+    has_many :comments, dependent: :destroy
+    validates :title, presence: true
+    validates :content, presence: true
+end
+  
