78f5bc22ae802af91d59a1177ec3c1b4836ceb81
KISHORE-2208 <kishoregunasekaran22@gmail.com>
2025-02-19 17:40:40 +0530

    Add files via upload

---
+++ controllers/posts_controller.rb
@@ -0,0 +1,72 @@
+class PostsController < ApplicationController
+    before_action :set_post, only: %i[show edit update destroy]
+  
+    def index
+        @posts = Post.all
+    end
+  
+    def show
+        @posts = Post.all
+    end
+  
+    def new
+        @post = Post.new
+    end
+  
+    def create
+        @post = Post.new(post_params)
+        if params[:post][:image].present?
+            @post.image.attach(params[:post][:image])
+        end
+      
+        if @post.save
+            redirect_to @post, notice: "Post was successfully created."
+        else
+            render :new
+        end
+    end
+      
+      
+  
+    def edit
+    end
+  
+    def update
+        if @post.update(post_params)
+            redirect_to @post, notice: 'Post was successfully updated.'
+        else
+            render :edit
+        end
+    end
+  
+    def destroy
+        @post.destroy
+        redirect_to posts_url, notice: 'Post was successfully deleted.'
+    end
+
+    def upload_image
+        @post = Post.find(params[:id])
+    end
+      
+    def save_image
+        @post = Post.find(params[:id])
+        if params[:post][:image].present?
+            @post.image.attach(params[:post][:image])
+            flash[:notice] = "Image uploaded successfully!"
+        else
+            flash[:alert] = "Please select an image."
+        end
+        redirect_to @post
+    end
+    
+    private
+    def post_params
+        params.require(:post).permit(:title, :content, :image)
+    end
+
+    def set_post
+        @post = Post.find(params[:id])
+    end
+    
+end
+  
