c921a3d9f7b0224a6f521c7bed70d3e531ad8479
Kishore M G <kishoregunasekaran22@gmail.com>
2025-03-04 23:06:48 +0530

    Added OpenAI API key configuration and updated environment settings

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,68 +2,64 @@ class PostsController < ApplicationController
     before_action :set_post, only: %i[show edit update destroy]
   
     def index
-        @posts = Post.all
+      @posts = Post.all
     end
   
     def show
-        @posts = Post.all
+      @posts = Post.all
     end
   
     def new
-        @post = Post.new
+      @post = Post.new
     end
   
     def create
-        @post = Post.new(post_params)
-        if @post.save
-          @post.image.attach(params[:post][:image]) if params[:post][:image].present?
-          redirect_to @post, notice: "Post was successfully created."
-        else
-          render :new
-        end      
+      @post = Post.new(post_params)
+      if @post.save
+        @post.image.attach(params[:post][:image]) if params[:post][:image].present?
+        puts "Post Content"
+        puts @post.content
+        redirect_to @post, notice: "Post was successfully created."
+      else
+        render :new
+      end      
     end
-      
+  
+    def preview
+        @post = Post.new  # Ensure @post is not nil
+        @preview_content = params[:content]
+        
+        respond_to do |format|
+          format.html { render :new }
+          format.json { render json: { content: @preview_content } }
+        end
+    end
       
   
     def edit
     end
   
     def update
-        if @post.update(post_params)
-            redirect_to @post, notice: 'Post was successfully updated.'
-        else
-            render :edit
-        end
+      if @post.update(post_params)
+        redirect_to @post, notice: 'Post was successfully updated.'
+      else
+        render :edit
+      end
     end
   
     def destroy
-        @post.destroy
-        redirect_to posts_url, notice: 'Post was successfully deleted.'
+      @post.destroy
+      redirect_to posts_url, notice: 'Post was successfully deleted.'
     end
-
-    def upload_image
-        @post = Post.find(params[:id])
-    end
-      
-    def save_image
-        @post = Post.find(params[:id])
-        if params[:post][:image].present?
-            @post.image.attach(params[:post][:image])
-            flash[:notice] = "Image uploaded successfully!"
-        else
-            flash[:alert] = "Please select an image."
-        end
-        redirect_to @post
-    end
-    
+  
     private
+  
     def post_params
-        params.require(:post).permit(:title, :content, :image)
+      params.require(:post).permit(:title, :content, :image)
     end
-
+  
     def set_post
-        @post = Post.find(params[:id])
+      @post = Post.find(params[:id])
     end
-    
 end
   
